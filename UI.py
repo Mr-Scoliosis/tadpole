52542fa01673de1c6385b0686b37a226f736e27c
Conor Claret <78763959+SuperFirecart@users.noreply.github.com>
2021-11-03 12:37:37 +0000

    Update UI.py
    Added self.frame and self.canvas

--- UI.py
+++ UI.py
@@ -1,32 +1,26 @@
-from tkinter import *;
+from tkinter import *
 class Task:
     def __init__(self, name, description, deadline):
         self.name = name
         self.description = description
         self.deadline = deadline
-
-
         #send task data to task database
+
 
 class LilyPad:
     def __init__(self, name):
         self.tasks = []
         self.name = name
-        self.canvas = Canvas
-        self.button = Button(self.canvas, text="add a lilypad", command=self.create_new)
-        self.button.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+        #send lilypad data to lilypad database
 
-        def create_new(self):
-            #upload new data to lilypad db
-            return
-            
 
 class Pond:
-    def __init__(self, name, Canvas):
+    def __init__(self, name, frame, canvas):
         self.lilypads = []
         self.pondname = name
-        self.canvas = Canvas
-        self.button = Button(self.canvas, text="create new pond", command=self.create_new)
+        self.frame = frame
+        self.canvas = canvas
+        self.button = Button(self.frame, text="+", command=self.create_new)
         self.button.grid(row=1, column=2, padx=10, pady=10, sticky="w")        
         
     def create_new(self):
@@ -35,21 +29,14 @@ class Pond:
 
 
 class Project:
-    def __init__(self, Canvas):
+    def __init__(self, frame, canvas):
         self.ponds = []
-        self.canvas = Canvas
-        Canvas.place(relx=0, rely=0, anchor="nw")
-        self.textbox = Text(self.canvas, height=1, width=15) 
         self.textbox.grid(row=len(self.ponds)+1,column=1, padx=10, pady=10, sticky="w")
-        self.createpro = Button(self.canvas, text="create new project", command=self.create)
         self.createpro.grid(row=len(self.ponds)+1,column=2, padx=10, pady=10, sticky="w")
 
     def create(self):
         #create a pond based on the name in the textbox
-        if self.textbox.get("1.0", END) == " ":
-            print("problem")
-            return
-        self.button = Button(self.canvas, text=self.textbox.get("1.0", END), command=self.show_Ponds)
+        self.button = Button(self.frame, text=self.textbox.get("1.0", END), command=self.show_Ponds)
         self.textbox.delete("1.0", END)
         self.button.grid(row=len(self.ponds)+1,column=1, padx=10, pady=10, sticky="w")
         #send data to database
@@ -59,12 +46,14 @@ class Project:
         self.createpro.grid(row=len(self.ponds)+1)
 
     def show_Ponds(self):
+        self.configure()
         #focus on this project and display all ponds involved
         for i in self.ponds:
             print(i)
-        createPond = Pond("Name", self.canvas)
+        createPond = Pond("Name", self.frame, self.canvas)
         self.ponds.append(createPond)
         #add a generate random code thing for team invites
+
 
 class TadPole():
     def __init__(self, root):
@@ -88,14 +77,14 @@ class TadPole():
         self.User1.grid(row=1,column=1, padx=10, pady=10)
         # self.Buttons.append(self.User1)
         self.User2 = Button(self.frame, text="Member", command=self.member)
-        self.User2.grid(row=1,column=3, padx=10, pady=10)
+        self.User2.grid(row=1,column=2, padx=10, pady=10)
         # self.Buttons.append(self.User2)
         
 
     def leader(self):
         self.User1.destroy()
         self.User2.destroy()
-        self.Projects.append(Project(self.frame))
+        self.Projects.append(Project(self.frame, self.can))
 
 
     def member(self):
@@ -105,11 +94,11 @@ class TadPole():
         self.Label1 = Label(self.frame, text="This is what the Team Member sees")
         self.Label1.grid(row=1, column=1, padx=10, pady=10, sticky="w")
         
-        self.Label2 = Label(self.frame, text="I am still a God")
-        self.Label2.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+        # self.Label2 = Label(self.frame, text="I am still a God")
+        # self.Label2.grid(row=2, column=1, padx=10, pady=10, sticky="w")
         
-        self.add_new = Button(self.frame, text="+", command=self.create)
-        self.add_new.grid(row=3,column=1, padx=10, pady=10, sticky="w")
+        # self.add_new = Button(self.frame, text="+", command=self.create)
+        # self.add_new.grid(row=3,column=1, padx=10, pady=10, sticky="w")
 
 
 
