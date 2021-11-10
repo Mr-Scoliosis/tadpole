eade3f940ba57791d6f2cfb4db4613ecd6079e06
guzzmle <73357188+guzzmle@users.noreply.github.com>
2021-11-10 14:18:12 +0000

    backend v4
    whoops 🥴

--- backend.py
+++ backend.py
@@ -8,9 +8,10 @@ connection = db.connect(
     database='cs2208_ld8')
 cursor = connection.cursor(db.cursors.DictCursor)
 
-def downloadProjects(self):
-    # connection
 
+
+
+def downloadProjects(self):
     cursor.execute("""SELECT * FROM Projects""")
     for row in cursor.fetchall():
         id = row['ID']
@@ -18,31 +19,64 @@ def downloadProjects(self):
         name = row['Name']
         self.Projects.append(Project(id, leader, name, self.frame, self.can))
 
+    def get_project_by_id(self, project_id):
+        print(self.Projects)
+        for i in self.Projects:
+            if i.id == project_id:
+                return i
+    return
+
+
+
+def downloadTeams(project):
     cursor.execute("""SELECT * FROM Teams""")
     for row in cursor.fetchall():
         id = row['ID']
         name = row['Name']
         project_id = row['Project_ID']
-        project_above = self.get_project_by_id(project_id)
-        project_above.addMembers(Team(id, name, self.can, project_above))
+        project_above = project.get_project_by_id(project_id)
+        project_above.addMembers(Team(id, name, project.can, project_above))
+    return
 
+
+
+def downloadPonds(project):
     cursor.execute("""SELECT * FROM Ponds""")
     for row in cursor.fetchall():
         id = row['ID']
         name = row['Name']
         project_id = row['Project_ID']
-        project_above = self.get_project_by_id(project_id)
-        project_above.addPond(Pond(id, name, self.can, project_above))
+        project_above = project.get_project_by_id(project_id)
+        project_above.addPond(Pond(id, name, project.can, project_above))
 
+    def get_pond_by_id(self, pond_id):
+        print(self.ponds)
+        for i in self.ponds:
+            if i.id == pond_id:
+                return i
+    return
+
+
+
+def downloadLilypads(pond):
     cursor.execute("""SELECT * FROM Lilypads""")
     for row in cursor.fetchall():
         id = row['ID']
         name = row['Name']
         pond_id = row['Pond_ID']
-        project_above = self.get_project_by_id(pond_id)
-        project_above.addLilyPad(LilyPad(id, name, self.can, pond_above))
+        pond_above = pond.get_pond_by_id(pond_id)
+        pond_above.addLilyPad(LilyPad(id, name, pond.can, pond_above))
+
+    def get_lilypad_by_id(self, lilypad_id):
+        print(self.Projects)
+        for i in self.Projects:
+            if i.id == lilypad_id:
+                return i
+    return
 
 
+
+def downloadTasks(lilypad):
     cursor.execute("""SELECT * FROM Tasks""")
     for row in cursor.fetchall():
         id = row['ID']
@@ -50,47 +84,23 @@ def downloadProjects(self):
         descrpt = row["Descrpt"]
 	    deadline = row["Deadline"]
         status = row["Status"]
-        pond_id = row['Pond_ID']
-        project_above = self.get_project_by_id(pond_id)
-        project_above.addLilyPad(LilyPad(id, name, self.can, pond_above))
+        lilypad_id = row['Lilypad_ID']
+        lilypad_above = lilypad.get_lilypad_by_id(lilypad_id)
+        lilypad_above.addLilyPad(LilyPad(id, name, lilypad.can, lilypad_above))
+    return
+
+
+
+
+connection.commit()
+cursor.close()
+connection.close()
 
 
     # testMember = member("Testing", [], self.memberFrame)
     # testMember2 = member("Testing12", [], self.memberFrame)
     # for i in self.Projects:
     #     i.add_to_members(testMember)
-
-
-    def get_project_by_id(self, project_id):
-        print(self.Projects)
-        for i in self.Projects:
-            if i.id == project_id:
-                return i
-
-    def get_pond_by_id(self, pond_id):
-        print(self.ponds)
-        for i in self.ponds:
-            if i.id == project_id:
-                return i
-
-    def get_lilypad_by_id(self, lilypad_id):
-        print(self.Projects)
-        for i in self.Projects:
-            if i.id == project_id:
-                return i
-
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return
-
-
-
-
-
-
-from frontend import *
-
 
     # return
     #these are to demonstrate downloading the projects from a database
